89ad60ec4b9693d85d1ef11b453aab00f0f102a0
Jakub Żmuda <goodguykuba@gmail.com>
2019-12-19 19:28:08 +0100

    input image

--- src/main/java/app/Lab5.java
+++ src/main/java/app/Lab5.java
@@ -1,14 +1,67 @@
 package app;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Lab5 {
-    public Lab5(Image image, App app) {
 
+    private final Image image;
+    private final App app;
+
+    ImageView inputImageView;
+
+    public Lab5(Image image, App app) {
+        this.image = image;
+        this.app = app;
+        this.inputImageView = new ImageView(image);
     }
 
     public void segmentationSplitAndMerge() {
+        Stage stage = new Stage();
+        stage.setTitle("Segmentacja: Dziel i łącz");
 
+        HBox inputImage = buildInputImageBox();
+
+        VBox container = new VBox(inputImage);
+
+        Scene scene = new Scene(container, 1200, 1000);
+
+        container.setAlignment(Pos.CENTER);
+        container.setSpacing(16);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    private HBox buildInputImageBox() {
+        HBox box = new HBox(new Label("Obraz wejściowy"), buildImageContainer(this.inputImageView));
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(16);
+        return box;
+    }
+
+    private StackPane buildImageContainer(ImageView imageView) {
+        int size = 300;
+        imageView.setFitHeight(size);
+        imageView.setFitWidth(size);
+        StackPane stackImageView = new StackPane(imageView);
+        ScrollPane imageContainer = new ScrollPane(imageView);
+        imageContainer.setPrefWidth(size);
+        imageContainer.setMaxWidth(size);
+        imageContainer.setPrefHeight(size);
+        imageContainer.setMaxHeight(size);
+        imageContainer.setFitToHeight(true);
+        imageContainer.setFitToWidth(true);
+        stackImageView.setAlignment(Pos.CENTER);
+        return stackImageView;
     }
 
     public void segmentationGrow() {
